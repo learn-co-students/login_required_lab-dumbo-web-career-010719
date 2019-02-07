1740d007f2552e5d8653461757d24946ee314249
NorbertLam <noobertlm@gmail.com>
2019-02-07 17:17:04 -0500

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,15 @@
+class SecretsController < ApplicationController
+  # before_action :require_login
+
+  def show
+    if current_user.nil?
+      redirect_to '/login'
+    end
+  end
+
+  private
+ 
+  def require_login
+    return head(:forbidden) unless session.include? :name
+  end
+end
